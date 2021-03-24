c32188ee0404b2c64a7093521c6dccc801f95f05
Wip-Sama <47224042+Wip-Sama@users.noreply.github.com>
2021-03-25 00:38:47 +0100

    banane

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,3 @@
-
 local entity_list = {
   "locomotive",
   "car",
@@ -6,55 +5,41 @@ local entity_list = {
   "mining-drill",
   "boiler",
   "furnace",
-  "reactor"
+  "reactor",
 }
+--[
+if mods["Krastorio2"] then
+  table.insert(entity_list, "generator-equipment")
+  table.insert(entity_list, "spider-vehicle")
+end
+--]]
 
 for _, entity in pairs(entity_list) do
 
   local function ChangeTable(name)
 
     if data.raw[entity][name] and data.raw[entity][name].burner then
-      if data.raw[entity][name].burner.fuel_category == "chemical" then
-      data.raw[entity][name].burner.fuel_category = nil
-      data.raw[entity][name].burner.fuel_categories = {"chemical", "IF"}
+      if data.raw[entity][name].burner.fuel_categories then
+        table.insert(data.raw[entity][name].burner.fuel_categories, "IF")
       end
-      --krastorio 2
-      if mods["Krastorio2"] and settings.startup["IF-kr-Infinite-Fuel"].value == true then
-        if data.raw[entity][name].burner.fuel_category == "vehicle-fuel" then
+      if data.raw[entity][name].burner.fuel_category then
+        data.raw[entity][name].burner.fuel_categories = {data.raw[entity][name].burner.fuel_category}
+        table.insert(data.raw[entity][name].burner.fuel_categories, "IF")
         data.raw[entity][name].burner.fuel_category = nil
-        data.raw[entity][name].burner.fuel_categories = {"vehicle-fuel", "IF"}
-        end
-        if data.raw[entity][name].burner.fuel_category == "nuclear-fuel" then
-        data.raw[entity][name].burner.fuel_category = nil
-        data.raw[entity][name].burner.fuel_categories = {"nuclear-fuel", "IF"}
-        end
       end
     end
 
     if data.raw[entity][name] and data.raw[entity][name].energy_source then
-      if data.raw[entity][name].energy_source.fuel_category == "chemical" then
-      data.raw[entity][name].energy_source.fuel_category = nil
-      data.raw[entity][name].energy_source.fuel_categories = {"chemical", "IF"}
+      if data.raw[entity][name].energy_source.fuel_categories then
+        table.insert(data.raw[entity][name].energy_source.fuel_categories, "IF")
       end
-      --nuclear reactor
-      if settings.startup["IF-enable-on-nuclear-reactor"].value == true then
-        if data.raw[entity][name].energy_source.fuel_category == "nuclear" then
+      if data.raw[entity][name].energy_source.fuel_category then
+        data.raw[entity][name].energy_source.fuel_categories = {data.raw[entity][name].energy_source.fuel_category}
+        table.insert(data.raw[entity][name].energy_source.fuel_categories, "IF")
         data.raw[entity][name].energy_source.fuel_category = nil
-        data.raw[entity][name].energy_source.fuel_categories = {"nuclear", "IF"}
-        end
-      end
-      --krastorio 2
-      if mods["Krastorio2"] and settings.startup["IF-kr-Infinite-Fuel"].value == true then
-        if data.raw[entity][name].energy_source.fuel_category == "vehicle-fuel" then
-        data.raw[entity][name].energy_source.fuel_category = nil
-        data.raw[entity][name].energy_source.fuel_categories = {"vehicle-fuel", "IF"}
-        end
-        if data.raw[entity][name].energy_source.fuel_category == "nuclear-fuel" then
-        data.raw[entity][name].energy_source.fuel_category = nil
-        data.raw[entity][name].energy_source.fuel_categories = {"nuclear-fuel", "IF"}
-        end
       end
     end
+
   end
 
   for _, list in pairs(data.raw[entity]) do
